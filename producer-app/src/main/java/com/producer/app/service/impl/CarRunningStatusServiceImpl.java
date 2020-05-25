c3dc4b4617d8e26bd6bd9381ecc9a1449d57183a
kuldeep <kuldeep.ya9@gmail.com>
2020-05-25 14:13:45 +0530

    demo

--- producer-app/src/main/java/com/producer/app/service/impl/CarRunningStatusServiceImpl.java
+++ producer-app/src/main/java/com/producer/app/service/impl/CarRunningStatusServiceImpl.java
@@ -1,6 +1,9 @@
 package com.producer.app.service.impl;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.transaction.Transactional;
 
@@ -16,15 +19,50 @@ public class CarRunningStatusServiceImpl implements CarRunningStatusService {
 
 	//@Autowired
 	//private ModelMapper modelMapper;
-
+	Random rand = new Random();
 	@Autowired
 	private Sender sender;
+	
+	
 
 	@Override
 	@Transactional
 	public void sendCarRunningInfo(List<CarRunningStatusDTO> carRunningStatusDTOList) {
 
-		carRunningStatusDTOList.forEach(carRunningStatusDTO -> sender.send(carRunningStatusDTO));
+	
+
+		// nextInt as provided by Random is exclusive of the top value so you need to add 1 
+
+		
+		int c=1;
+		List<Integer> latlong=new ArrayList<>();
+		 
+		for(int i=5;i<=10;i++) {
+			try {
+			int ramdomNum=generateRandom(i,c);
+			int ramdomNum2=generateRandom(i+1,c);
+			
+			latlong.add(ramdomNum);
+			latlong.add(ramdomNum2);		
+			 sender.send( new CarRunningStatusDTO("",latlong,LocalDateTime.now()));
+			// carRunningStatusDTOList.forEach(carRunningStatusDTO -> sender.send(carRunningStatusDTO));
+			 latlong.removeAll(latlong);
+			}
+			catch(Exception e) {
+				System.out.println(e.getStackTrace());
+			}
+		}
+		
+		
 
 	}
+	
+	public Integer generateRandom(int max,int min) {
+		
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+		
+		
+	}
+	
 }
